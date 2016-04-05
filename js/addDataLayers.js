77abb4e9bb4884f5d469da57d87aba288bbd6418
air2637 <air2637@gmail.com>
2016-04-05 10:14:35 +0800

    added some data layers

--- js/addDataLayers.js
+++ js/addDataLayers.js
@@ -23,6 +23,49 @@ var atmDataLayer = new ol.layer.Vector({
     style: atmIconStyle
 });
 
+var cafeDataLayer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: 'data/cafe.geojson',
+        format: new ol.format.GeoJSON()
+    })
+});
+
+var communityCentreDataLayer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: 'data/communityCenter.geojson',
+        format: new ol.format.GeoJSON()
+    })
+});
+
+var foodBeverageDataLayer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: 'data/food_beverage.geojson',
+        format: new ol.format.GeoJSON()
+    })
+});
+
+var libraryDataLayer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: 'data/library.geojson',
+        format: new ol.format.GeoJSON()
+    })
+});
+
+var parkingDataLayer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: 'data/parking.geojson',
+        format: new ol.format.GeoJSON()
+    })
+});
+
+var taxiDataLayer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: 'data/taxi.geojson',
+        format: new ol.format.GeoJSON()
+    })
+});
+
+// register listening event
 
 $("#natural_data_layer").change(function() {
     if ($("#natural_data_layer").prop('checked') == true) {
@@ -46,3 +89,50 @@ $("#atm_data_layer").change(function() {
 });
 
 
+$("#cafe_data_layer").change(function() {
+    if ($("#cafe_data_layer").prop('checked') == true) {
+        map.addLayer(cafeDataLayer);
+    } else {
+        map.removeLayer(cafeDataLayer);
+    }
+});
+
+$("#communityCentre_data_layer").change(function() {
+    if ($("#communityCentre_data_layer").prop('checked') == true) {
+        map.addLayer(communityCentreDataLayer);
+    } else {
+        map.removeLayer(communityCentreDataLayer);
+    }
+});
+
+$("#foodBeverage_data_layer").change(function() {
+    if ($("#foodBeverage_data_layer").prop('checked') == true) {
+        map.addLayer(foodBeverageDataLayer);
+    } else {
+        map.removeLayer(foodBeverageDataLayer);
+    }
+});
+
+$("#library_data_layer").change(function() {
+    if ($("#library_data_layer").prop('checked') == true) {
+        map.addLayer(libraryDataLayer);
+    } else {
+        map.removeLayer(libraryDataLayer);
+    }
+});
+
+$("#parking_data_layer").change(function() {
+    if ($("#parking_data_layer").prop('checked') == true) {
+        map.addLayer(parkingDataLayer);
+    } else {
+        map.removeLayer(parkingDataLayer);
+    }
+});
+
+$("#taxi_data_layer").change(function() {
+    if ($("#taxi_data_layer").prop('checked') == true) {
+        map.addLayer(taxiDataLayer);
+    } else {
+        map.removeLayer(taxiDataLayer);
+    }
+});
