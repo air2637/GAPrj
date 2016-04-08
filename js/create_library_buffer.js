16fdebf9dc0c641b16844899565fafbb8be0886a
air2637 <air2637@gmail.com>
2016-04-08 19:20:26 +0800

    lib_buffer on/off is done

--- js/create_library_buffer.js
+++ js/create_library_buffer.js
@@ -1,84 +1,43 @@
 // create_library_buffer.js
 
-/*var pt = {
-    type: "Feature",
-    properties: {},
-    geometry: {
-        type: "Point",
-        coordinates: [103.872095, 1.357983]
-    }
-};
-
-
 var unit = 'meters';
+var radius = 35;
+var lib_buffer_layer;
+var testNum = 0;
 
-var buffered = turf.buffer(pt, 35, unit);
-var result = turf.featurecollection([buffered.features, pt]);
+function add_buffer_to_lib(radius, unit) {
+    $.getJSON("data/library.geojson", function(data) {
 
+        var buffer_object = turf.buffer(data, radius, unit);
+        //console.log(JSON.stringify(buffer_object));
 
+        var formater = new ol.format.GeoJSON({
+            defaultDataProjection: 'EPSG:4326',
+            projection: 'EPSG:4326'
+        });
 
-var formater = new ol.format.GeoJSON({
-    defaultDataProjection: 'EPSG:4326',
-    projection: 'EPSG:4326'
-});
-
-var vectorSource = new ol.source.Vector({
-    features: formater.readFeatures(buffered)
-});
-
-var vectorLayer = new ol.layer.Vector({
-    source: vectorSource
-});
-
-map.addLayer(vectorLayer);*/
+        var vectorSource = new ol.source.Vector({
+            features: formater.readFeatures(buffer_object)
+        });
 
 
-// get every library potes
-var unit = 'meters';
-var buffered = [];
-var features = [];
+        window.lib_buffer_layer = new ol.layer.Vector({
+            source: vectorSource
+        });
 
-$.getJSON("data/library.geojson", function(data) {
-    var items = [];
-    $.each(data, function(key, val) {
-        //items.push( "<li id='" + key + "'>" + val + "</li>" );
+        window.testNum = 1;
+        map.addLayer(window.lib_buffer_layer);
+        return window.lib_buffer_layer;
 
-        if (key == "features") {
-            //features = val;
-            $.each(val, function(key, value) {
-                /*console.log("in");
-                console.log(key + ": " + value);*/
-                buffered.push(turf.buffer(value, 35, unit));
-            });
-        }
     });
+}
 
+$("#lib_buffer_layer").change(function() {
+    if ($("#lib_buffer_layer").prop('checked') == true) {
+
+        lib_buffer_layer = add_buffer_to_lib(radius, unit);
+
+    } else {
+        map.removeLayer(lib_buffer_layer);
+    }
 });
-console.log("before");
-
-$.each(features, function(key, value) {
-    console.log("in");
-    alert(key + ": " + value);
-});
-console.log("after");
-
-var buffered = turf.buffer(features, 35, unit);
-
-var formater = new ol.format.GeoJSON({
-    defaultDataProjection: 'EPSG:4326',
-    projection: 'EPSG:4326'
-});
-
-var vectorSource = new ol.source.Vector({
-    features: formater.readFeatures(buffered)
-});
-
-var vectorLayer = new ol.layer.Vector({
-    source: vectorSource
-});
-
-map.addLayer(vectorLayer);
-
-// create buffer zone for each pointer
-
-// add the buffer layer to map
