4ee8861c6aa80734f9c070198efa1a2316f817e2
air2637 <air2637@gmail.com>
2016-04-15 16:29:46 +0800

    change the index page layout

--- js/showPointsInBufferZone.js
+++ js/showPointsInBufferZone.js
@@ -5,7 +5,7 @@ function showPointsInBufferZone(pointLayer, polygonLayer) {
 
     // convert lib_buffer multipolygon to a collection of polygons
 
-    var polygonLayer = lib_buffer_layer;
+    //var polygonLayer = lib_buffer_layer;
     var geojson = new ol.format.GeoJSON();
     var polygon_obj = geojson.writeFeaturesObject(polygonLayer.getSource().getFeatures());
     //console.log(JSON.stringify(polygon_obj));
@@ -21,25 +21,45 @@ function showPointsInBufferZone(pointLayer, polygonLayer) {
     });
     var polygons_obj = {'type': 'FeatureCollection', 'features': features};*/
 
+    var point_json_file_dir = '';
+    switch(pointLayer){
+        case 'Cafe':
+            point_json_file_dir = 'data/cafe.geojson';
+            break;
+        case 'ATM':
+             point_json_file_dir = 'data/atm.geojson';
+            break;
+        case 'F&B':
+             point_json_file_dir = 'data/food_beverage.geojson';
+            break;
+        case 'Parking':
+            point_json_file_dir = 'data/parking.geojson';
+            break;
+        case 'Taxi':
+            point_json_file_dir = 'data/taxi.geojson';
+            break;
+        default:
+            //do nothing
+    }
 
     //while reading cafe point data and do the count computation with polygon
-    $.getJSON('data/cafe.geojson', function(data) {
+    $.getJSON(point_json_file_dir, function(data) {
         var point_obj = data;
         //counted is the polygon object with pt_count attribute attached
         var counted = turf.count(polygon_obj, point_obj, 'pt_count');
         console.log(JSON.stringify(counted));
-        
+
         var resultFeatures = point_obj.features.concat(counted.features);
         var result = {
             "type": "FeatureCollection",
             "features": resultFeatures
         };
-        console.log(JSON.stringify(result));
+        //console.log(JSON.stringify(result));
     });
 
 }
 
 
 $("#lib_buffer_button").click(function() {
-    showPointsInBufferZone(0, 0);
+    showPointsInBufferZone("ATM", lib_buffer_layer);
 });
